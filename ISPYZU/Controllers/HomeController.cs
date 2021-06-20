3dd734e97f3bdcb8f1a25e0a4ff83a3cd2f685f7
HM Nomaan <hasannoman31@gmail.com>
2021-06-20 16:40:10 +0600

    Revert "little changes for performance"
    This reverts commit f0063cf1d424835df7d7b244c9b161e0fcee3317.

--- ISPYZU/Controllers/HomeController.cs
+++ ISPYZU/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,22 +9,11 @@ namespace ISPYZU.Controllers
 {
     public class HomeController : Controller
     {
-        
         public ActionResult Index()
         {
             return View();
         }
-        [ChildActionOnly]
-        [OutputCache(Duration = 15)]
-        public PartialViewResult MyViewStuff()
-        {
-            _DoBackEndStuff();
-            return PartialView();
-        }
-        private void _DoBackEndStuff()
-        {
-            Thread.Sleep(1000);
-        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
